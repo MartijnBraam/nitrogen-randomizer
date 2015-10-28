633f963c610a83859740f1b3c74dedb97be4a5a5
Martijn Braam <martijn@brixit.nl>
2015-10-28 17:32:23 +0100

    Added inline documentation

--- nitrogen-randomizer.py
+++ nitrogen-randomizer.py
@@ -7,6 +7,7 @@ import time
 import random
 from subprocess import call
 
+# Define and parge command line arguments
 parser = argparse.ArgumentParser(description="Wallpaper randomizer for nitrogen")
 parser.add_argument("--nitrogen-config-dir", "-c",
                     help="Configuration directory used by nitrogen (defaults to ~/.config/nitrogen)",
@@ -18,28 +19,41 @@ args = parser.parse_args()
 
 
 def swap_wallpapers(config_dir, wallpaper_dir):
+    """Set a random wallpaper for every display and reload"""
+    
+    # Get all files in the configured wallpaper directory and randomize the order
     wallpapers = list(glob.glob(wallpaper_dir + "/*"))
     random.shuffle(wallpapers)
 
+    # Load the nitrogen config file
     nitrogen_config = configparser.ConfigParser()
     nitrogen_config.read(os.path.join(config_dir, "bg-saved.cfg"))
 
     monitors = nitrogen_config.sections()
     for monitor in monitors:
+        # Set the first wallpaper from the randomized list as wallpaper
+        # for the monitor and remove from the list
         nitrogen_config[monitor]["file"] = wallpapers[0]
         wallpapers = wallpapers[1:]
+        
+    # Save the nitrogen config file
     with open(os.path.join(config_dir, "bg-saved.cfg"), "w") as configfile:
         nitrogen_config.write(configfile)
 
+    # Call nitrogen to reload the file
     call(["nitrogen", "--restore"])
 
 
 if __name__ == "__main__":
+    # Expand the tilde in the paths to the users homedirectory
     config_dir = os.path.expanduser(args.nitrogen_config_dir)
     wallpaper_pool = os.path.expanduser(args.wallpaperpool)
+    
+    # Set some random wallpapers as soon as this tool is started
     swap_wallpapers(config_dir, wallpaper_pool)
     while not args.once:
         try:
+            # Wait the configured period before changing the wallpaper
             time.sleep(args.interval)
             swap_wallpapers(config_dir, wallpaper_pool)
         except KeyboardInterrupt:
